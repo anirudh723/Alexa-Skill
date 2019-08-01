a4ad94351c7e67095ccaa7bd086c4ff99a550baf
Anirudh Singh <anirudh.s723@gmail.com>
2019-08-01 16:53:29 -0700

    created two helper functions to handle the different types of requests. Need to create a statement function which returns the response Alexa will provide, both verbally and on the cards.

--- CSWiki.py
+++ CSWiki.py
@@ -4,3 +4,23 @@ def lambda_handler(event, context):
         return on_launch(event, context)
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event, context)
+
+
+# Launch
+def on_launch(event, context):
+    return statement("Greetings", "Welcome to the CS Wiki!")
+
+
+# Intent Routing
+def on_intent(event, context):
+    intent = event['request']['intent']['name']
+    if intent == "AskDefinitionIntent":
+        return ask_definition_intent(event, context)
+    if intent == "AMAZON.CancelIntent":
+        return cancel_intent()
+    if intent == "AMAZON.HelpIntent":
+        return help_intent()
+    if intent == "AMAZON.StopIntent":
+        return stop_intent()
+    else:
+        return statement("Please try again", "I could not recognize a request for a definition. Please ask again.")
